086336aa6db9e007043d19d2b9964e985a35b401
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-05-28 22:03:06 -0500

    HangmanGame: Configuración Inicial Webpack y SASS

---
+++ README.md
@@ -0,0 +1,14 @@
+ # Hangman Game
+
+ "Juego del Ahorcado"
+
+ ### Notas:
+ Recuerden reconstruir los módulos de Node
+ ```
+ npm install
+ ```
+
+ Y para construir el build, recuerden:
+ ```
+ npm run build
+ ```
---
+++ src/js/componentes.js
@@ -0,0 +1,14 @@
+// import './../css/componentes.css';
+
+const eventos = () => {
+    console.log('Event Listeners');
+};
+
+const init = () => {
+    console.log('Hangman Game');
+    eventos();
+};
+
+export {
+    init
+}
